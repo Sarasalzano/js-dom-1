3dcd14328b8ee034e0f16a98269617988b3a0ecb
Sara Salzano <sarasalzano95@gmail.com>
2025-09-04 17:09:07 +0200

    esegui esercizio

---
+++ script.js
@@ -0,0 +1,39 @@
+//SELEZIONE
+const lampadinaSpenta = document.getElementById("lamp-off");
+console.log(lampadinaSpenta);
+
+const lampadinaAccesa = document.getElementById("lamp-on");
+console.log(lampadinaAccesa);
+
+//GESTIAMO EVENTO SU BOTTONE
+bottoneAccensione.addEventListener('click',
+    accendiLampadina
+);
+
+//SVOLGO LA FUNZIONE DI ACCENSIONE LAMPADINA
+function accendiLampadina () {
+    //check classe aggiuntiva
+    const isItHidden = lampadinaSpenta.classList.contains("d-none");
+    //se la classe è gia presente, allora rimuovila
+    if (isItHidden) {
+        lampadinaSpenta.classList.remove("d-none");
+        //altrimenti aggiungila
+    } else {
+        lampadinaSpenta.classList.add("d-none");
+    }
+    
+    //check classe aggiuntiva
+    const isItVisible = lampadinaAccesa.classList.contains("d-block");
+    if (isItVisible) {
+        lampadinaAccesa.classList.remove("d-block");
+        
+    } else {
+        lampadinaAccesa.classList.add("d-block");
+    } 
+
+    console.log(lampadinaSpenta.classList);
+    console.log(lampadinaAccesa.classList);
+
+}
+
+
